2624132dd0fac49b732101d2cde54296769a2692
Tushar-18 <112742512+Tushar-18@users.noreply.github.com>
2023-07-22 18:29:39 +0530

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.css">
+    {{-- @vite('resources/css/app.css') --}}
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="post" class="form-box">
+            <div class="logo"><img src="Images/logo.png" alt="error"></div><br>
+            <div class="comp"><br>
+                <p for="" class="l-login">Sign in with an Aretuza Games account</p><br>
+                <div class="input-box">
+                    <input class="inputs" type="text" placeholder="Email"><br>
+                    <input class="inputs" type="password" placeholder="Password" id="myInput"><br>
+                </div>
+                <div class="pwd">
+                    <div class="show"><input type="checkbox" onclick="show()">Show password</div>
+                    <a href="">forgot password</a>
+                </div>
+                <input type="button" value="LOG IN NOW" class="submint-btn"><br>
+                <p style="color: #e6dfdf;"> Don't have an account? <a href="" style="color: #fff;">sign up</a>
+                </p><br>
+            </div>
+        </form>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+     <div class="container">
+        <header>
+            <div class="logo"><img src="Images/logo.png " alt=""></div>
+            <nav>
+                <ul>
+                    <li ><a href="" class="active">Store</a></li>
+                    <li><a href="" class="nav-hover">Distribute</a></li>
+                    <li><a href="" class="nav-hover">Engine</a></li>
+                    <li><a href="" class="nav-hover">Engine</a></li>
+                </ul>
+            </nav>
+            <div class="profile">
+                
+            </div>
+        </header>
+    </div>
+</body>
+</html>
