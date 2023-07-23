b48cac8bfd575cdc6f02e3bba9d67e38d36efb6f
Tushar-18 <112742512+Tushar-18@users.noreply.github.com>
2023-07-23 12:16:02 +0530

    navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,4 +1,4 @@
-    <!DOCTYPE html>
+    {{-- <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -16,13 +16,68 @@
                     <li ><a href="" class="active">Store</a></li>
                     <li><a href="" class="nav-hover">Distribute</a></li>
                     <li><a href="" class="nav-hover">Engine</a></li>
-                    <li><a href="" class="nav-hover">Engine</a></li>
+                    <li><a href="" class="nav-hover">offers</a></li>
                 </ul>
             </nav>
             <div class="profile">
+                <div class="pro-in">
+                    <img src="Images/default.jpg" alt="error" id="signin">
+                    <div class="log"><a href="">Login</a></div>
+                </div>
+                <label for="signin" style="margin-right: 10px;color:#fff;">Name</label>
+                <div class="menutoggle">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
                 
             </div>
         </header>
     </div>
 </body>
-</html>
+</html> --}}
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="css/nav.css">
+
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        {{-- @vite('resources/css/app.css') --}}
+    </head>
+
+    <body>
+        <div class="container">
+            <header class="header active">
+                <div class="logo"><img src="Images/logo.png" alt="error"></div>
+                <nav class="nav">
+                    <ul>
+                        <li ><a class="nav-hover" href="">Home</a></li>
+                        <li ><a class="nav-hover" href="">store</a></li>
+                        <li ><a class="nav-hover" href="">Engine</a></li>
+                    </ul>
+                </nav>
+                <div class="profile">
+                    <div class="pro-in">
+                        <img src="Images/default.jpg" alt="error" id="signin">
+                        <div class="log"><a href="">Login</a></div>
+                    </div>
+                    <label for="signin" style="margin-right: 10px">Name</label>
+                    <div class="menutoggle">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
+            </header>
+        </div>
+        <script src="js/nav.js"></script>
+    </body>
+
+    </html>
