45b6a53906edea7dac3d7f51d49915fbda7308d3
HarshitNananiya <hnananiya813@rku.ac.in>
2023-07-23 10:19:48 +0530

    add

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <script src="https://cdn.tailwindcss.com"></script>
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+   <div class="images">
+      <div class="box-border bg-gradient-to-r from-black p1">
+         <div class="text-large box-border text-white">
+            Aretuza
+         </div>
+         <div class="text-small box-border text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, itaque non modi assumenda in maxime repellendus minima reprehenderit distinctio dolore, dolorum ut adipisci architecto aliquam velit iure quo! Doloribus, doloremque?
+       </div>
+      </div>
+    </div>
+    <div class="box-border ">
+      div.
+    </div>
+    
+</body>
+</html>
