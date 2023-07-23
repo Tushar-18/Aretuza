d4eedfb9bb073e709936f5249fdcc90619a6bb8e
Tushar-18 <112742512+Tushar-18@users.noreply.github.com>
2023-07-23 13:53:01 +0530

    navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,22 +1,26 @@
-    {{-- <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="css/nav.css">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- @vite('resources/css/app.css') --}}
 </head>
+
 <body>
-     <div class="container">
-        <header>
-            <div class="logo"><img src="Images/logo.png " alt=""></div>
-            <nav>
+    <div class="container-main">
+        <header class="header ">
+            <div class="logo"><img src="Images/logo.png" alt="error"></div>
+            <nav class="nav">
                 <ul>
-                    <li ><a href="" class="active">Store</a></li>
-                    <li><a href="" class="nav-hover">Distribute</a></li>
-                    <li><a href="" class="nav-hover">Engine</a></li>
-                    <li><a href="" class="nav-hover">offers</a></li>
+                    <li><a class="active-btn" href="">Home</a></li>
+                    <li><a class="nav-hover" href="">store</a></li>
+                    <li><a class="nav-hover" href="">Engine</a></li>
                 </ul>
             </nav>
             <div class="profile">
@@ -24,60 +28,16 @@
                     <img src="Images/default.jpg" alt="error" id="signin">
                     <div class="log"><a href="">Login</a></div>
                 </div>
-                <label for="signin" style="margin-right: 10px;color:#fff;">Name</label>
+                <label for="signin" style="margin-right: 10px;color:white;">Name</label>
                 <div class="menutoggle">
                     <div></div>
                     <div></div>
                     <div></div>
                 </div>
             </div>
-                
-            </div>
         </header>
     </div>
+    <script src="js/nav.js"></script>
 </body>
-</html> --}}
 
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="css/nav.css">
-
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        {{-- @vite('resources/css/app.css') --}}
-    </head>
-
-    <body>
-        <div class="container">
-            <header class="header active">
-                <div class="logo"><img src="Images/logo.png" alt="error"></div>
-                <nav class="nav">
-                    <ul>
-                        <li ><a class="nav-hover" href="">Home</a></li>
-                        <li ><a class="nav-hover" href="">store</a></li>
-                        <li ><a class="nav-hover" href="">Engine</a></li>
-                    </ul>
-                </nav>
-                <div class="profile">
-                    <div class="pro-in">
-                        <img src="Images/default.jpg" alt="error" id="signin">
-                        <div class="log"><a href="">Login</a></div>
-                    </div>
-                    <label for="signin" style="margin-right: 10px">Name</label>
-                    <div class="menutoggle">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </header>
-        </div>
-        <script src="js/nav.js"></script>
-    </body>
-
-    </html>
+</html>
