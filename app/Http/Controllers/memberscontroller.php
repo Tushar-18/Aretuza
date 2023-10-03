a86dd7e63008ebfcac3716392a8e5b96710406b6
HarshitNananiya <hnananiya813@rku.ac.in>
2023-10-03 13:23:07 +0530

    add

--- app/Http/Controllers/memberscontroller.php
+++ app/Http/Controllers/memberscontroller.php
@@ -224,6 +224,7 @@ public function admin_add_user_reg(Request $req){
         return view('edit_profile',compact('data'));
     }
     public function edit_profile(Request $req){
+        return 'hello'; 
         $req->validate([
             'name' => 'required|max:20|min:2',
             'dob' => 'required',
