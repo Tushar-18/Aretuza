c2c53883ca2dafbe83ed370a94a9086910892ee5
HarshitNananiya <hnananiya813@rku.ac.in>
2023-11-03 14:57:50 +0530

    add

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -19,21 +19,22 @@
                 <p class="text-5xl text-white p-5">Library</p>
             </div>
             <div class="box-border first-part bg-zinc-800 rounded-md">
-               
+
                 @foreach ($data as $d)
-                @if (session('email')==$d['email'])
-                    <div class="box-border games">
-                        <div class="games-img m-3">
-                            <img src="{{ URL::to('/') }}/images/game_pic/{{ $d['game_pic'] }}" alt="errer">
-                        </div>
-                        <div class="games-name">
-                            <p class="big">Remnant</p>
-                            <p class="small text-white"> price: ₹400</p>
-                            <a href="download_pdf"
-                                class="bg-red-700 items-center my-2 flex w-full h-10 justify-center rounded-lg text-white hover:bg-red-800 hover:transition delay-75 duration-300 ease-in-out hover:-translate-y-1">Invoice</a>
-                        </div>
-                    </div>
-                @endif
+                    @if (session('email') == $d['email'])
+                        <a href="{{ URL::to('/') }}/items_a/{{ $d['game_id'] }}">
+                            <div class="box-border games">
+                                <div class="games-img m-3">
+                                    <img src="{{ URL::to('/') }}/images/game_pic/{{ $d['game_pic'] }}" alt="errer">
+                                </div>
+                                <div class="games-name">
+                                    <p class="big">{{ $d['game_name'] }}</p>
+                                        <a href="download_pdf"
+                                            class="bg-red-700 items-center my-2 flex w-full h-10 justify-center rounded-lg text-white hover:bg-red-800 hover:transition delay-75 duration-300 ease-in-out hover:-translate-y-1">Invoice</a>
+                                </div>
+                            </div>
+                        </a>
+                    @endif
                 @endforeach
             </div>
         </div>
