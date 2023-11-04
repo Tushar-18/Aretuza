213ead135afc6ab49af1599e37e42b83f0bd22f6
HarshitNananiya <hnananiya813@rku.ac.in>
2023-11-04 10:10:07 +0530

    add

--- resources/views/register_template.blade.php
+++ resources/views/register_template.blade.php
@@ -1,13 +1,7 @@
-if ($reg->save()) {
-     session()->flash('succ', 'Data saved successfully');
-     $email = $req->em;
-     $fn = $req->fn;
-     $data = ['em' => $email, 'fn' => $fn];
-     Mail::send('register_template', ["data1" => $data], function ($message) use ($data) {
+Hello, {{ $data1['fn'] }}
 
-         $message->to($data['em'], $data['fn']);
-         $message->from("travaliya519@rku.ac.in", "Tushar");
-     });
- } else {
-     session()->flash('err', 'error in saving data');
- }
+Congratulations your account is created successfully.
+
+Please click on the link below to activate your account.
+
+http://127.0.0.1:8000/account_activation/{{ $data1['em'] }}
